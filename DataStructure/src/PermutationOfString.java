e06d023c7ac35ed6d3ca976c88a3deb7ef692618
raghav235 <Raghav.Mishra235@gmail.com>
2022-03-13 17:25:28 +0530

    Update PermutationOfString.java

--- DataStructure/src/PermutationOfString.java
+++ DataStructure/src/PermutationOfString.java
@@ -1,33 +1,15 @@
-
-public class PermutationOfString {
-
-	public static void main(String[] args) {
-
-		String s="abc";
-		permutation(s);
-	}
-	
-	private static void permutation(String s) {
-		permutation(s,"");
-	}
-	/**
-	 * 
-	 * @param rotateMe
-	 * @param fixed
-	 * 
-	 * Fixed char 0, rotate rest of string
-	 * For rest of String , fix char 0 , rotate rest of string
-	 * 
-	 */
-	public static void permutation(String rotateMe, String fixed){
-		if(rotateMe.isEmpty()){
-			System.out.println(fixed+"");
-		}
-		for(int i=0;i<rotateMe.length();i++){
-			char ch= rotateMe.charAt(i);
-			String toBeRotated= rotateMe.substring(0,i)+rotateMe.substring(i+1);
-			permutation(toBeRotated, fixed+ch);
-		}
-	}
-
+public class Test {
+    public static void main(String[] args) {
+        String s="abc";
+        printallPermutation(s, "");
+    }
+    private static void printallPermutation(String s, String result ) {
+        if(s.isEmpty()) {
+            System.out.println(result);
+            return;
+        }
+        for(int i=0;i<s.length();i++) {
+            printallPermutation(new String(s.substring(0,i)+s.substring(i+1)), result+s.charAt(i));
+        }
+    }
 }
